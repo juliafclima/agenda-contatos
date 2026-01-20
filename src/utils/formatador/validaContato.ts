ee066682dd5e64a230a2af0128f8562a227b1ae9
Júlia Lima <juliafclima@hotmail.com>
2026-01-20 08:30:00 -0300

    feat: validacao mínimo de duas palavras, cada uma com pelo menos 3 letras.

--- src/utils/formatador/validaContato.ts
+++ src/utils/formatador/validaContato.ts
@@ -9,6 +9,9 @@ export const validaContato = (
       return "Nome é obrigatório e deve ser uma string!";
     }
 
+    const erroNome = validaNome(data.nome);
+    if (erroNome) return erroNome;
+
     if (!data.telefone || typeof data.telefone !== "string") {
       return "Telefone é obrigatório e deve ser uma string!";
     }
@@ -19,9 +22,33 @@ export const validaContato = (
       return "Nome deve ser uma string!";
     }
 
+    if (data.nome) {
+      const erroNome = validaNome(data.nome);
+      if (erroNome) return erroNome;
+    }
+
     if (data.telefone && typeof data.telefone !== "string") {
       return "Telefone deve ser uma string!";
     }
+  }
+
+  return null;
+};
+
+const validaNome = (nome: string): string | null => {
+  const nomeTratado = nome.trim().replace(/\s+/g, " ");
+  const palavras = nomeTratado.split(" ");
+
+  if (palavras.length < 2) {
+    return "Nome deve conter pelo menos duas palavras";
+  }
+
+  const palavraInvalida = palavras.some(
+    (palavra) => palavra.length < 3 || !/^[A-Za-zÀ-ÖØ-öø-ÿ]+$/.test(palavra),
+  );
+
+  if (palavraInvalida) {
+    return "Cada palavra do nome deve ter pelo menos 3 letra";
   }
 
   return null;
